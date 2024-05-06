ce863b8bf20abf173e2597949cea83ae1f1593ef
Dhiya-Natarajan <dhiya005.natarajan@gmail.com>
2024-05-06 13:09:39 +0530

    first commit

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,26 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import "./style.css"
+import { TodoList } from "./TodoList"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        { <img src={logo} className="App-logo" alt="logo" /> }
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Created by Dhiya Natarajan
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App()
+ {
+  const [todos, settodos] =useState(()=>{
+    const localValue =localStorage.getItem("ITEMS")
+    if(localValue == null)
+      return []
+    return JSON.parse(localValue)
+  })
 
-export default App;
+  useEffect(()=>{
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+  
+ 
+  function addTodo(title)
+   {
+     settodos((currentTodos)=>{
+      return[
+     ...currentTodos,
+     { id: crypto.randomUUID(),title,completed: false },
+    ]
+  })
+  }
+
+  function toggleTodo(id, completed)
+  {
+    settodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id===id){
+          return { ...todo, completed}
+        }
+
+        return todo
+
+      })
+    })
+  }
+
+    function deleteTodo(id){
+      settodos(currentTodos => {
+        return currentTodos.filter(todo => todo.id !== id)
+      })
+    
+  }
+  
+   return (
+    <div className="centered">
+      <NewTodoForm onSubmit ={addTodo}/>
+      <h1 className="header">Hi There,your</h1>
+      <h2 className="header">ToDo List</h2>
+      <h3 className="header"> </h3>
+      
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      </div>
+      )
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   import logo from './logo.svg';
+//  import './App.css';
+
+//   function App() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           { <img src={logo} className="App-logo" alt="logo" /> }
+//          <p>
+//            Edit <code>src/App.js</code> and save to reload.
+//            Created by Dhiya Natarajan
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//          </a>
+//         </header>
+//       </div>
+//     );
+//   }
+
+//   export default App
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// function HelloWorld()
+// {
+//   return <h1 className="greeting">Hello, world!</h1>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
